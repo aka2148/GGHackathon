3f8c5c9e0f8c8195b988495d2d10d412394f2277
aka2148 <ak2006feb7@gmail.com>
2026-04-10 17:12:57 +0530

    Initial commit creating the repository

---
+++ src/main/java/com/gridgarrison/gridgarrison/GridgarrisonApplication.java
@@ -0,0 +1,13 @@
+package com.gridgarrison.gridgarrison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridgarrisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridgarrisonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gridgarrison/gridgarrison/GridgarrisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gridgarrison.gridgarrison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridgarrisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
